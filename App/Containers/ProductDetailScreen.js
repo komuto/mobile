ac8eedd992deb28fadf30f82429a4eb72c42c381
Beny Liantriana <Beny@users-MacBook-Air.local>
2017-07-27 14:26:57 +0700

    testing commit

--- App/Containers/ProductDetailScreen.js
+++ App/Containers/ProductDetailScreen.js
@@ -1318,7 +1318,7 @@ class ProductDetailScreenScreen extends React.Component {
           </TouchableOpacity>
           <TouchableOpacity style={styles.buttonOke} onPress={() => this.beliSekarang()}>
             <Text style={styles.labelButtonOke}>
-              Beli Sekarang
+              Beli Sekarang JUGAAA
             </Text>
           </TouchableOpacity>
         </View>

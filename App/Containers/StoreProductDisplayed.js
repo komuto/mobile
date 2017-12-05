e2ef6e06e093b43c0562374d0fc82dd701a243d4
Beny Liantriana <Beny@beny-2.local>
2017-12-05 19:35:46 +0700

    fix commission

--- App/Containers/StoreProductDisplayed.js
+++ App/Containers/StoreProductDisplayed.js
@@ -160,7 +160,7 @@ class StoreProductDisplayed extends React.Component {
 
   labeldaridropshipper (data) {
     if (data.is_dropship === true && data.dropship_origin) {
-      var commisson = data.dropship_origin.commission.nominal * 100
+      var commisson = data.dropship_origin.commission.nominal
       var maskedCommision = this.maskedMoney(commisson)
       return (
         <View>
